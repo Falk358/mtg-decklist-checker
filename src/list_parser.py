34e031d6a8478837143af51677145f49dc33294f
Falk358 <maxdanelli@gmail.com>
2025-12-17 18:08:55 +0100

    created main.py and list parser mod, test load file

---
+++ src/list_parser.py
@@ -0,0 +1,11 @@
+import re
+
+
+def read_file_to_string(filename: str) -> str:
+    with open(filename, "r") as file:
+        return file.read()
+
+
+def parse_into_lines(file: str) -> list[str]:
+    lines_parsed: list[str] = [line for line in file.splitlines()]
+    empty_lines_removed: list[str] = filter(lambda x: x == "", lines_parsed)
---
+++ tests/test_parser.py
@@ -0,0 +1,41 @@
+import re
+
+import pytest
+
+import list_parser
+
+
+@pytest.fixture
+def parse_line_in_file_incorrect():
+    return "44fdafad"
+
+
+@pytest.fixture
+def parse_line_in_file_correct():
+    return "38 Mountain"
+
+
+def test_each_line_starts_with_number(parse_line_in_file_correct: str):
+    deck_list_entry_regex = re.compile(r"^[0-9]{1,2}\s.*$")
+    match: re.Match = deck_list_entry_regex.match(parse_line_in_file_correct)
+    assert match is not None
+
+
+def test_incorrect_line_fails(parse_line_in_file_incorrect):
+    deck_list_entry_regex = re.compile(r"^[0-9]{1,2}\s.*$")
+    match: re.Match[str] | None = deck_list_entry_regex.match(parse_line_in_file_incorrect)
+    assert match is None
+
+
+@pytest.fixture
+def file_path_test_list() -> str:
+    return "tests/test_data/test_list_mono_red.txt"
+
+
+def test_read_file(file_path_test_list: str):
+    import src.list_parser
+
+    list_as_string: str = list_parser.read_file_to_string(file_path_test_list)
+    assert list_as_string is not None
+    assert type(list_as_string) == str
+    assert len(list_as_string) > 0
