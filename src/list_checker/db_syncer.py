96a5aa015f4def374d80d67f2830d2bd8ffb7fae
Falk358 <maxdanelli@gmail.com>
2026-01-02 16:21:18 +0100

    read from json file; copy needed fields to list of dicts

--- src/list_checker/db_syncer.py
+++ src/list_checker/db_syncer.py
@@ -32,3 +32,19 @@ def insert_card_info_db(engine: Engine, card_info_obj: dict):
     with Session(engine) as session:
         session.add(card_orm_obj)
         session.commit()
+
+
+def read_from_json(file_path: str) -> list[dict]:
+    import ijson
+
+    list_of_cards: list[dict] = []
+    with open(file_path) as json_file:
+        for record in ijson.items(json_file, "item"):
+            record_processed: dict = {
+                "id": record["id"],
+                "name": record["name"],
+                "game_changer": record["game_changer"],
+                "legalities": record["legalities"],
+            }
+            list_of_cards.append(record_processed)
+    return list_of_cards
--- tests/test_db_syncer.py
+++ tests/test_db_syncer.py
@@ -83,3 +83,26 @@ def test_insert_card_info_db(db_file_path: str, card_info_test_example: dict):
         conn.close()
         os.remove(db_file_path)
     assert not os.path.exists(db_file_path)
+
+
+@pytest.fixture
+def json_file_path() -> str:
+    return os.path.join(os.path.dirname(__file__), "test_resources/test_card_list.json")
+
+
+def test_read_from_json(json_file_path: str):
+    from list_checker.db_syncer import read_from_json
+
+    result: list[dict] = read_from_json(json_file_path)
+    assert len(result) == 3
+    for item in result:
+        assert type(item) == dict
+        assert "id" in item.keys()
+        assert "name" in item.keys()
+        assert "game_changer" in item.keys()
+        assert "legalities" in item.keys()
+        assert type(item["name"]) == str
+        assert type(item["game_changer"]) == bool
+        assert type(item["legalities"]) == dict
+        assert len(item["legalities"].keys()) == 21
+        assert len(item.keys()) == 4
