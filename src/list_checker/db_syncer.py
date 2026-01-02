17a7f484c2f55a852c334fbc81e0c61345e35410
Falk358 <maxdanelli@gmail.com>
2026-01-02 16:41:23 +0100

    defined index for name column (will be searched frequently)

--- src/list_checker/db_syncer.py
+++ src/list_checker/db_syncer.py
@@ -14,7 +14,7 @@ class CardLegality(Base):
     __tablename__ = "card_legalities"
 
     id: Mapped[uuid.UUID] = MappedColumn(primary_key=True)
-    name: Mapped[str] = MappedColumn()
+    name: Mapped[str] = MappedColumn(index=True)
     game_changer: Mapped[bool] = MappedColumn()
     legalities: Mapped[dict[str, str]] = MappedColumn()
 
