91e52f9749ea6c4795f759778b95ec03716cab79
Falk358 <maxdanelli@gmail.com>
2026-01-02 14:11:46 +0100

    save orm object to db

--- src/list_checker/db_syncer.py
+++ src/list_checker/db_syncer.py
@@ -1,21 +1,34 @@
 import os
-import sqlite3
-from sqlalchemy import Column, Integer, String, JSON, create_engine, Table, MetaData, Boolean
+import uuid
+
+from sqlalchemy import Column, String, JSON, create_engine, Table, MetaData, Boolean, Uuid
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, MappedColumn, Session
+
+
+class Base(DeclarativeBase):
+    type_annotation_map = {dict[str, str]: JSON}
+
+
+class CardLegality(Base):
+    __tablename__ = "card_legalities"
+
+    id: Mapped[uuid.UUID] = MappedColumn(primary_key=True)
+    name: Mapped[str] = MappedColumn()
+    game_changer: Mapped[bool] = MappedColumn()
+    legalities: Mapped[dict[str, str]] = MappedColumn()
 
 
 def init_db(file_path: str):
     if os.path.exists(file_path):
         raise FileExistsError(f"[ERROR] DB: {file_path} already exists; skipping init")
     engine = create_engine(f"sqlite:///{file_path}")
-    table_schema = Table(
-        "card_legalities",
-        MetaData(),
-        Column("id", Integer, primary_key=True),
-        Column("name", String(125), nullable=False),
-        Column("game_changer", Boolean),
-        Column("legalities", JSON),
-    )
-    with engine.connect() as conn:
-        table_schema.create(conn)
-        conn.commit()
+    CardLegality.metadata.create_all(engine)
     return engine
+
+
+def insert_card_info_db(engine: Engine, card_info_obj: dict):
+    card_orm_obj = CardLegality(**card_info_obj)
+    with Session(engine) as session:
+        session.add(card_orm_obj)
+        session.commit()
--- tests/test_db_syncer.py
+++ tests/test_db_syncer.py
@@ -1,6 +1,10 @@
 import os
+from uuid import UUID
 
 import pytest
+from sqlalchemy.orm import Session
+
+from list_checker.db_syncer import CardLegality
 
 
 @pytest.fixture
@@ -9,7 +13,6 @@ def db_file_path() -> str:
 
 
 def test_db_initated(db_file_path: str):
-    print(f"file_path from fixture: {db_file_path}")
     from list_checker.db_syncer import init_db
 
     engine = init_db(db_file_path)
@@ -23,6 +26,59 @@ def test_db_initated(db_file_path: str):
         assert insp.has_table("card_legalities")
         for curr_col in insp.get_columns("card_legalities"):
             assert curr_col["name"] in needed_column_names
+    finally:
+        conn.close()
+        os.remove(db_file_path)
+    assert not os.path.exists(db_file_path)
+
+
+@pytest.fixture
+def card_info_test_example() -> dict:
+    return {
+        "id": UUID(hex="3d69a3e0-6a2e-475a-964e-0affed1c017d"),
+        "name": "Birds of Paradise",
+        "game_changer": False,
+        "legalities": {
+            "standard": "not_legal",
+            "future": "not_legal",
+            "historic": "legal",
+            "timeless": "legal",
+            "gladiator": "legal",
+            "pioneer": "not_legal",
+            "modern": "legal",
+            "legacy": "legal",
+            "pauper": "not_legal",
+            "vintage": "legal",
+            "penny": "legal",
+            "commander": "legal",
+            "oathbreaker": "legal",
+            "standardbrawl": "not_legal",
+            "brawl": "legal",
+            "alchemy": "not_legal",
+            "paupercommander": "not_legal",
+            "duel": "legal",
+            "oldschool": "not_legal",
+            "premodern": "legal",
+            "predh": "legal",
+        },
+    }
+
+
+def test_insert_card_info_db(db_file_path: str, card_info_test_example: dict):
+    from list_checker.db_syncer import init_db
+    from list_checker.db_syncer import insert_card_info_db
+
+    engine = init_db(db_file_path)
+    try:
+        conn = engine.connect()
+        assert conn
+        insert_card_info_db(engine, card_info_test_example)
+        with Session(engine) as session:
+            result = session.get(CardLegality, card_info_test_example["id"])
+            assert result
+            assert result.id == card_info_test_example["id"]
+            assert result.name == "Birds of Paradise"
+
     finally:
         conn.close()
         os.remove(db_file_path)
